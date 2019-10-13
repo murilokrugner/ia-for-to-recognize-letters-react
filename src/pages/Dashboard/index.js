fd88cb4021a7887dcdfe6902aeed29b10457b8e6
murilokrugner <murilokrugner@hotmail.com>
2019-10-13 18:34:49 -0300

    axios

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from "react";
 import { createWorker } from "tesseract.js";
 import test from "../../assets/por.png";
 
-import { Container } from "./styles";
+import api from "../../services/api";
+
+import { Container, Date } from "./styles";
 
 export default function Dashboard() {
+  const [ocr, setOcr] = useState("Recognizing...");
+  const [user, setUser] = useState([]);
+
   const worker = createWorker({
     logger: m => console.log(m)
   });
@@ -19,10 +24,20 @@ export default function Dashboard() {
     setOcr(text);
   };
 
-  const [ocr, setOcr] = useState("Recognizing...");
   useEffect(() => {
     doOCR();
-  });
+    async function loadUser() {
+      const response = await api.get("users", {
+        params: { name: "Murilo" }
+      });
+
+      setUser(response.data);
+    }
+
+    loadUser();
+  }, [doOCR]);
+
+  console.log(user);
 
   return (
     <Container>
