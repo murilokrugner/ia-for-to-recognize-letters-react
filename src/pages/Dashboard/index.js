7feb073923b5244a1334e0a9a40925a129111c26
murilokrugner <murilokrugner@hotmail.com>
2019-10-22 16:28:56 -0300

    styles frontend

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,62 +1,85 @@
 import React, { useEffect, useState } from "react";
+import HashLoader from "react-spinners/HashLoader";
 import { createWorker } from "tesseract.js";
 import test from "../../assets/murilo.png";
 import { Form, Input } from "@rocketseat/unform";
+import signature from "../../assets/icon.svg";
 
 import api from "../../services/api";
 
-import { Container, Date, Result } from "./styles";
+import { Container, Loading, File, Button, User } from "./styles";
 
 export default function Dashboard() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState();
   const [ocr, setOcr] = useState([]);
   const [user, setUser] = useState([]);
 
-  useEffect(() => {
-    async function doOCR() {
-      const worker = createWorker({
-        logger: m => console.log(m)
-      });
-      await worker.load();
-      await worker.loadLanguage("por");
-      await worker.initialize("por");
-      const {
-        data: { text }
-      } = await worker.recognize(test);
-      setOcr(text);
-      if (!text) {
-        setLoading(true);
-      } else {
-        setLoading(false);
-      }
-    }
+  function handleImage(e) {
+    const data = e.target.files[0];
 
-    doOCR();
-  }, [ocr]);
+    setImage(data);
+  }
+
+  console.log(image);
+
+  async function worker() {
+    setLoading(true);
+    const worker = createWorker({
+      logger: m => console.log(m)
+    });
+    await worker.load();
+    await worker.loadLanguage("por");
+    await worker.initialize("por");
+    const {
+      data: { text }
+    } = await worker.recognize(image);
+    setOcr(text);
+    setLoading(false);
+  }
+
+  function handleSubmit() {
+    worker();
+  }
 
   const result = ocr;
 
-  async function handleSubmit() {
+  async function handleApi() {
     const response = await api.get(`users/${result}`);
     setUser(response.data);
-    console.log(response.data);
   }
 
   return (
     <Container>
+      <h1>Verifique a assinatura</h1>
+      <File>
+        <label htmlFor="file">
+          <img src={signature} alt="file" />
+          <input type="file" id="file" onChange={handleImage} />
+        </label>
+      </File>
       {loading ? (
-        <Date>
-          <strong>Carregando...</strong>
-        </Date>
+        <Loading>
+          <HashLoader color="#fff" />
+        </Loading>
       ) : (
         <>
-          <Form onSubmit={handleSubmit}>
-            <strong>Nome processado: {ocr}</strong>
-            <br />
-            <button type="submit">Verificar autenticidade</button>
-          </Form>
+          <strong>{ocr}</strong>
         </>
       )}
+      <Button>
+        <button className="b1" type="button" onClick={() => handleSubmit()}>
+          Verificar
+        </button>
+        <button className="b2 " type="button" onClick={() => handleApi()}>
+          Obter os dados
+        </button>
+      </Button>
+      {user.map(user => (
+        <User key={user}>
+          <strong>{user.name}</strong>
+        </User>
+      ))}
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,7 +1,82 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 
-export const Date = styled.div``;
+  h1 {
+    margin-top: 100px;
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+  }
 
-export const Result = styled.div``;
+  strong {
+    margin-top: 100px;
+    color: #fff;
+  }
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  margin-top: 30px;
+  justify-content: center;
+`;
+
+export const File = styled.div`
+  align-self: center;
+  margin-bottom: 30px;
+  label {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.7;
+    }
+    img {
+      margin-top: 30px;
+      height: 50px;
+      width: 50px;
+    }
+
+    input {
+      display: none;
+    }
+    input.type.file {
+      color: #000;
+    }
+  }
+`;
+
+export const Button = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  button.b1 {
+    margin: 40px 0 0;
+    height: 50px;
+    width: 100px;
+    background: #ff2f84;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+
+  button.b2 {
+    margin-top: 40px;
+    margin-left: 20px;
+    height: 50px;
+    width: 100px;
+    background: #ff2f84;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const User = styled.div``;
